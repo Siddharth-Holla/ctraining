08aef790830358f65ac63a6954c8d52c6adf2107
Vijay Jayaram <vijjay12@in.ibm.com>
2026-06-16 13:20:32 +0530

    remove top level c programs

--- p3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    char s[50];
-
-    printf("enter name of person: ");
-    scanf("%s",s);
-    printf("the name of the person is: %s\n",s);
-}
--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int i;
-    if(i==4){
-        printf("Hello World");  
-}
-    return 0;
-}
